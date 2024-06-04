d18063743322010d8e08bfaf3187b3ba8eed0347
lniz <168931018+lniz@users.noreply.github.com>
2024-06-04 16:14:43 -0700

    Add files via upload

--- Final Project Code/139_DESCISIONTREE2.py
+++ Final Project Code/139_DESCISIONTREE2.py
@@ -12,7 +12,7 @@ Boosting_data = pd.read_csv("Data/BoostingData.csv")
 
 X = Boosting_data.values[:,3:]
 y = Boosting_data.values[:,2]
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=random_state)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=random_state)
 X_train = np.asarray(X_train.astype("float32"))
 X_test = np.asarray(X_test.astype("float32"))
 
@@ -25,6 +25,7 @@ from xgboost import XGBRegressor
 bdt = XGBRegressor(n_estimators=100,max_depth=3,learning_rate=0.1,colsample_bytree=0.8, subsample=0.8)
 print(bdt)
 bdt.fit(X_train, y_train)
+preds_bdt_train= bdt.predict(X_train)
 preds_bdt = bdt.predict(X_test)
 
 
@@ -41,21 +42,18 @@ plt.show()
 
 #second descision tree
 
-X2= X_test
-y2= np.array([list(preds_bdt) , list(y_test)]).transpose()
-
-X2_train, X2_test, y2_train, y2_test = train_test_split(X2, y2, test_size=0.3, random_state=random_state)
+'''X2_train, X2_test, y2_train, y2_test = train_test_split(X2, y2, test_size=0.3, random_state=random_state)
 X2_train = np.asarray(X2_train.astype("float32"))
-X2_test = np.asarray(X2_test.astype("float32"))
+X2_test = np.asarray(X2_test.astype("float32"))'''
 
-y_error_train= y2_train[:,1]-y2_train[:,0]
+y_error_train= y_train - preds_bdt_train 
 
-y_error_test= y2_test[:,1]-y2_test[:,0]
+y_error_test= y_test - preds_bdt 
 
 bdt2 = XGBRegressor(n_estimators=100,max_depth=3,learning_rate=0.1,colsample_bytree=0.8, subsample=0.8)
 print(bdt2)
-bdt2.fit(X2_train, y_error_train)
-preds_bdt2 = bdt2.predict(X2_test)
+bdt2.fit(X_train, y_error_train)
+preds_bdt2 = bdt2.predict(X_test)
 
 
 plt.scatter(preds_bdt2, y_error_test)
@@ -76,7 +74,7 @@ plt.show()
 
 
 
-plt.scatter(y2_test[:,0] + preds_bdt2, y2_test[:,1])
+plt.scatter(preds_bdt2 + preds_bdt, y_test)
 plt.plot(np.arange(200, 400), np.arange(200,400), color= "r")
 plt.xlabel('Predicted energy values') 
 plt.ylabel('Actual energy values')
